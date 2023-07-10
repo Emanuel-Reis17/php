9ea60666bd9034dfcba0d07456a46ce1a27ddf80
Emanuel Reis <emanuelemreis@gmail.com>
2023-07-10 12:12:40 -0300

    exclusion

--- udemy/metodos/get/cad.php
+++
@@ -1,3 +0,0 @@
-<p>A Nome é: <?php echo $_GET["nome"];?></p>
-<p>A Email é: <?php echo $_GET["email"];?></p>
-<p>A Senha é: <?php echo $_GET["senha"];?></p>
--- udemy/metodos/post/cad.php
+++
@@ -1,3 +0,0 @@
-<p>A Nome é: <?php echo $_POST["nome"];?></p>
-<p>A Email é: <?php echo $_POST["email"];?></p>
-<p>A Senha é: <?php echo $_POST["senha"];?></p>
